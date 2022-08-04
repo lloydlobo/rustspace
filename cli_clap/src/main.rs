c034256ea766559e86b7f0ef7217e21de3c0ff03
Lloyd Lobo <lloyd@lloydlobo.com>
2022-08-05 00:27:53 +0530

    chore(cli_clap): format main.rs

--- cli_clap/src/main.rs
+++ cli_clap/src/main.rs
@@ -1,25 +1,24 @@
-
 // https://github.com/clap-rs/clap/tree/91b10554f419318599b53b1c9f18f14a93935ee7/examples/tutorial_builder
 
-    use clap::Parser;
+use clap::Parser;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-   /// Name of the person to greet
-   #[clap(short, long, value_parser)]
-   name: String,
+    /// Name of the person to greet
+    #[clap(short, long, value_parser)]
+    name: String,
 
-   /// Number of times to greet
-   #[clap(short, long, value_parser, default_value_t = 1)]
-   count: u8,
+    /// Number of times to greet
+    #[clap(short, long, value_parser, default_value_t = 1)]
+    count: u8,
 }
 
 fn main() {
-   let args = Args::parse();
+    let args = Args::parse();
 
-   for _ in 0..args.count {
-       println!("Hello {}!", args.name)
-   }
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
